36bbdb7b9156010099ee075bfac82a8e1acfba13
floralshopppe <stefanalecs003@yahoo.com>
2025-02-28 17:47:37 +0200

    add get_character_count function

--- stats.py
+++ stats.py
@@ -8,3 +8,14 @@ def get_num_words(book_path: str):
     book_text = get_book_text(book_path)
     num_words = len(book_text.split())
     return num_words
+
+
+def get_character_count(book_text: str):
+    character_dict = {}
+    for character in book_text:
+        character = character.lower()
+        if character in character_dict:
+            character_dict[character] += 1
+        else:
+            character_dict[character] = 1
+    return character_dict
