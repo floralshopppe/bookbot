dc970c842124082994ecd525386e711553037621
floralshopppe <stefanalecs003@yahoo.com>
2025-02-28 16:45:02 +0200

    add get_num_words function

--- stats.py
+++ stats.py
@@ -2,3 +2,9 @@ def get_book_text(book_path: str):
     with open(book_path) as f:
         file_contents = f.read()
     return file_contents
+
+
+def get_num_words(book_path: str):
+    book_text = get_book_text(book_path)
+    num_words = len(book_text.split())
+    return num_words
